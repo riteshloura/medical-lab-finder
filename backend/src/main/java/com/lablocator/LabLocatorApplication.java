ff19f4be584a20fca355126d2524fdd0577b68e3
Ritesh <riteshloura9090@gmail.com>
2026-03-07 15:28:15 +0530

    Initial project setup

---
+++ backend/src/main/java/com/lablocator/LabLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.lablocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabLocatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lablocator/LabLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lablocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabLocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
